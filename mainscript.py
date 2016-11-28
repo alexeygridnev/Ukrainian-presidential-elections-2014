2878f6d793c3ddeab7edffd7a709dc9075aa871c
Aleksei Gridnev <alexeygridnev93@gmail.com>
2016-11-28 18:16:39 +0100

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -1,9 +1,9 @@
 import bs4
 import requests
-
+#getting data from all the polling stations of one district
 def getdata(url):
     site=requests.get(url)
-    if site.text.find("Інформація щодо цієї сторінки відсутня")>0:
+    if site.text.find("Інформація щодо цієї сторінки відсутня")>0: ##for districts with no data; mostly means occupied territories of Ukraine
         print('Skipped...')
     else:
         sitebs=bs4.BeautifulSoup(site.text)
@@ -14,7 +14,7 @@ def getdata(url):
         datastr[0]=datastr[0].replace('\n', '')
         datastr[0]=datastr[0].replace('\xa0', '')
         print(datastr[0])
-        file=open(datastr[0]+'.csv', encoding='utf-8', mode="w")
+        file=open(datastr[0]+'.csv', encoding='utf-8', mode="w") #naming a file by the name of the district and the region
         datastr[2]=datastr[2].replace(',', '')
         datastr[2]=datastr[2].replace('\n', ',')
         datastr[2]=datastr[2].rstrip(',')
@@ -29,5 +29,5 @@ def getdata(url):
             print(datastr[i])
             
 for j in range (1, 227):
-    getdata('http://www.cvk.gov.ua/pls/vp2014/wp336?pt001f01=702&pt005f01='+str(j))
+    getdata('http://www.cvk.gov.ua/pls/vp2014/wp336?pt001f01=702&pt005f01='+str(j)) ##getting data for all the 226 disrticts
     
